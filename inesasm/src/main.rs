481860603e1754b7826cae009ba08cb800811a10
Rakarake <williambodin2001@gmail.com>
2023-06-10 19:13:24 +0200

    Basic parser works

--- inesasm/src/main.rs
+++ inesasm/src/main.rs
@@ -1,34 +1,26 @@
 use nom::{
-  bytes::complete::{tag, take_while_m_n},
-  combinator::map_res,
-  sequence::Tuple,
+  bytes::complete::tag,
   IResult,
+  error::ParseError,
+  combinator::value,
+  bytes::complete::is_not,
+  character::complete::char,
+  multi::many0_count,
+  combinator::recognize,
+  sequence::pair,
+  character::complete::alpha1,
+  branch::alt,
+  character::complete::space0,
+  character::complete::multispace0,
+  character::complete::multispace1,
+  character::complete::alphanumeric1,
+  sequence::tuple,
+  combinator::opt,
+  multi::many0,
+  combinator::all_consuming,
 };
 
-#[derive(Debug, PartialEq)]
-pub struct Color {
-  pub red: u8,
-  pub green: u8,
-  pub blue: u8,
-}
-
-fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
-  u8::from_str_radix(input, 16)
-}
-
-fn is_hex_digit(c: char) -> bool {
-  c.is_digit(16)
-}
-
-fn hex_primary(input: &str) -> IResult<&str, u8> {
-  map_res(take_while_m_n(2, 2, is_hex_digit), from_hex)(input)
-}
-
-fn hex_color(input: &str) -> IResult<&str, Color> {
-  let (input, _) = tag("#")(input)?;
-  let (input, (red, green, blue)) = (hex_primary, hex_primary, hex_primary).parse(input)?;
-  Ok((input, Color { red, green, blue }))
-}
+// Addressing modes for LDA
 
 //Immediate     LDA #$44      $A9  2   2
 //Zero Page     LDA $44       $A5  2   3
@@ -39,6 +31,7 @@ fn hex_color(input: &str) -> IResult<&str, Color> {
 //Indirect,X    LDA ($44,X)   $A1  2   6
 //Indirect,Y    LDA ($44),Y   $B1  2   5+
 
+#[derive(Debug)]
 enum Operand {
     // Different adressing modes
     Implied,        //
@@ -52,11 +45,13 @@ enum Operand {
     IndirectY(u16), //($44),Y
 }
 
+#[derive(Debug)]
 enum Opcode {
     LDA,
     STA,
 }
 
+#[derive(Debug)]
 enum Statement {
     Operation(Opcode, Option<Operand>),
     // Directives
@@ -64,35 +59,48 @@ enum Statement {
     DataBytes(Vec<u8>),
 }
 
+#[derive(Debug)]
 enum DecoratedStatement {
     Label(String, Statement),
     NoLabel(Statement),
 }
 
+#[derive(Debug)]
 enum Program {
     Program(Vec<DecoratedStatement>),
 }
 
-use nom::branch::alt;
+// Parse an entire string of assembly
 fn parse_program(input: &str) -> IResult<&str, Program> {
-    unimplemented!()
+    // TODO: add support for newlines
+    let (input, _) = parse_multispace_comment(input)?;
+    let (input, statements) = all_consuming(many0(parse_decorated_statement))(input)?;
+    Ok((input, Program::Program(statements)))
 }
 
-use nom::character::complete::space0;
-use nom::character::complete::multispace0;
-use nom::character::complete::line_ending;
-use nom::character::complete::alpha1;
-use nom::sequence::tuple;
-use nom::combinator::opt;
 fn parse_label(input: &str) -> IResult<&str, &str> {
-    let (input, label) = alpha1(input)?;
+    let (input, label) = parse_identifier(input)?;
     let (input, _) = tag(":")(input)?;
     Ok((input, label))
 }
 
+// Taken from the nom documentation: 
+// https://docs.rs/nom/latest/nom/recipes/index.html#rust-style-identifiers
+pub fn parse_identifier(input: &str) -> IResult<&str, &str> {
+  recognize(
+    pair(
+      alt((alpha1, tag("_"))),
+      many0_count(alt((alphanumeric1, tag("_"))))
+    )
+  )(input)
+}
+
+// A decorated statement might have a label
 fn parse_decorated_statement(input: &str) -> IResult<&str, DecoratedStatement> {
     let (input, maybe_label) = opt(parse_label)(input)?;
+    let (input, _) = multispace0(input)?;
     let (input, statement) = parse_statement(input)?;
+    let (input, _) = parse_multispace_comment(input)?;
     let decorated_statement = match maybe_label {
         Some(label) => DecoratedStatement::Label(label.to_string(), statement),
         None => DecoratedStatement::NoLabel(statement)
@@ -102,11 +110,20 @@ fn parse_decorated_statement(input: &str) -> IResult<&str, DecoratedStatement> {
 
 fn parse_statement(input: &str) -> IResult<&str, Statement> {
     let (input, _) = space0(input)?;
-    let (input, statement) = alt((parse_operation,parse_directive))(input)?;
+    // TODO: add parse_operation
+    let (input, statement) = alt((parse_directive,parse_directive))(input)?;
     let (input, _) = alt((parse_comment, value((), space0)))(input)?;
     Ok((input, statement))
 }
 
+// Any number of spaces, newlines and comment
+fn parse_multispace_comment(input: &str) -> IResult<&str, ()> {
+    let (input, _) = many0(alt((value((), multispace1), parse_comment)))(input)?;
+    Ok((input, ()))
+}
+
+// Taken from the nom documentation
+// https://docs.rs/nom/latest/nom/recipes/index.html#comments
 fn parse_comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (), E> {
   value(
     (), // Output is thrown away.
@@ -116,46 +133,35 @@ fn parse_comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (),
 
 fn parse_directive(input: &str) -> IResult<&str, Statement> {
     let (input, _) = tag(".")(input)?;
-    !unimplemented!()
+    // TODO: add directives here with the alt combinator
+    parse_bing_chilling(input)
+}
+
+// The greatest test directive, gives off chills in the assembly
+fn parse_bing_chilling (input: &str) -> IResult<&str, Statement> {
+    let (input, (_, _, _)) = tuple((tag("bing"), space0, tag("\"chilling\"")))(input)?;
+    Ok((input, Statement::Bing))
 }
 
 fn parse_operation(input: &str) -> IResult<&str, Statement> {
     !unimplemented!()
 }
 
-use nom::{
-  error::ParseError,
-  combinator::value,
-  sequence::pair,
-  bytes::complete::is_not,
-  character::complete::char,
-};
-
 const TEST_1: &str = "#2F14DF";
 
 const TEST_2: &str = "
-    .bing \"chilling\"
-    LDA $40
-    TAX             ; Transfer A to X
-    STX $FFFF,X
+_SUPBRU_H: .bing \"chilling\"
+           .bing \"chilling\"
+MONSer:    .bing \"chilling\"
 ";
-
+    //LDA $40
+    //TAX             ; Transfer A to X
+    //STX $FFFF,X
 fn main() {
-  println!("{:?}", hex_color(TEST_1))
+  println!("{:?}", parse_program(TEST_2))
 }
 
 #[test]
 fn parse_color() {
-  assert_eq!(
-    hex_color(TEST_1),
-    Ok((
-      "",
-      Color {
-        red: 47,
-        green: 20,
-        blue: 223,
-      }
-    ))
-  );
 }
 
