f28526e61c7cb4834843d48a5ef49d43a6ffbb79
Rakarake <williambodin2001@gmail.com>
2023-06-12 20:27:26 +0200

    Parsing numbers maybe

--- inesasm/src/main.rs
+++ inesasm/src/main.rs
@@ -14,10 +14,16 @@ use nom::{
   character::complete::multispace0,
   character::complete::multispace1,
   character::complete::alphanumeric1,
+  character::complete::u8,
+  character::complete::u16,
   sequence::tuple,
   combinator::opt,
   multi::many0,
   combinator::all_consuming,
+  multi::many1,
+  sequence::{preceded, terminated},
+  character::complete::one_of,
+  combinator::map_res,
 };
 
 // Addressing modes for LDA
@@ -31,7 +37,7 @@ use nom::{
 //Indirect,X    LDA ($44,X)   $A1  2   6
 //Indirect,Y    LDA ($44),Y   $B1  2   5+
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Operand {
     // Different adressing modes
     Implied,        //
@@ -71,89 +77,97 @@ enum Program {
 }
 
 // Parse an entire string of assembly
-fn parse_program(input: &str) -> IResult<&str, Program> {
+fn parse_program(i: &str) -> IResult<&str, Program> {
     // TODO: add support for newlines
-    let (input, _) = parse_multispace_comment(input)?;
-    let (input, statements) = all_consuming(many0(parse_decorated_statement))(input)?;
-    Ok((input, Program::Program(statements)))
+    let (i, _) = parse_multispace_comment(i)?;
+    let (i, statements) = all_consuming(many0(parse_decorated_statement))(i)?;
+    Ok((i, Program::Program(statements)))
 }
 
-fn parse_label(input: &str) -> IResult<&str, &str> {
-    let (input, label) = parse_identifier(input)?;
-    let (input, _) = tag(":")(input)?;
-    Ok((input, label))
-}
-
-// Taken from the nom documentation: 
-// https://docs.rs/nom/latest/nom/recipes/index.html#rust-style-identifiers
-pub fn parse_identifier(input: &str) -> IResult<&str, &str> {
-  recognize(
-    pair(
-      alt((alpha1, tag("_"))),
-      many0_count(alt((alphanumeric1, tag("_"))))
-    )
-  )(input)
+fn parse_label(i: &str) -> IResult<&str, &str> {
+    let (i, label) = parse_identifier(i)?;
+    let (i, _) = tag(":")(i)?;
+    Ok((i, label))
 }
 
 // A decorated statement might have a label
-fn parse_decorated_statement(input: &str) -> IResult<&str, DecoratedStatement> {
-    let (input, maybe_label) = opt(parse_label)(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, statement) = parse_statement(input)?;
-    let (input, _) = parse_multispace_comment(input)?;
+fn parse_decorated_statement(i: &str) -> IResult<&str, DecoratedStatement> {
+    let (i, maybe_label) = opt(parse_label)(i)?;
+    let (i, _) = multispace0(i)?;
+    let (i, statement) = parse_statement(i)?;
+    let (i, _) = parse_multispace_comment(i)?;
     let decorated_statement = match maybe_label {
         Some(label) => DecoratedStatement::Label(label.to_string(), statement),
         None => DecoratedStatement::NoLabel(statement)
     };
-    Ok((input, decorated_statement))
+    Ok((i, decorated_statement))
 }
 
-fn parse_statement(input: &str) -> IResult<&str, Statement> {
-    let (input, _) = space0(input)?;
+fn parse_statement(i: &str) -> IResult<&str, Statement> {
+    let (i, _) = space0(i)?;
     // TODO: add parse_operation
-    let (input, statement) = alt((parse_directive, parse_operation))(input)?;
-    let (input, _) = alt((parse_comment, value((), space0)))(input)?;
-    Ok((input, statement))
+    let (i, statement) = alt((parse_directive, parse_operation))(i)?;
+    let (i, _) = alt((parse_comment, value((), space0)))(i)?;
+    Ok((i, statement))
 }
 
-// Any number of spaces, newlines and comment
-fn parse_multispace_comment(input: &str) -> IResult<&str, ()> {
-    let (input, _) = many0(alt((value((), multispace1), parse_comment)))(input)?;
-    Ok((input, ()))
-}
-
-// Taken from the nom documentation
-// https://docs.rs/nom/latest/nom/recipes/index.html#comments
-fn parse_comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (), E> {
-  value(
-    (), // Output is thrown away.
-    pair(char('%'), is_not("\n\r"))
-  )(i)
-}
-
-fn parse_directive(input: &str) -> IResult<&str, Statement> {
-    let (input, _) = tag(".")(input)?;
+fn parse_directive(i: &str) -> IResult<&str, Statement> {
+    let (i, _) = tag(".")(i)?;
     // TODO: add directives here with the alt combinator
-    parse_bing_chilling(input)
-}
-
-fn parse_opcode(input: &str) -> IResult<&str, Opcode> {
-    alt((
-        value(Opcode::LDA, tag("LDA")),
-        value(Opcode::STA, tag("STA"))
-    ))(input)
+    parse_bing_chilling(i)
 }
 
 // The greatest test directive, gives off chills in the assembly
-fn parse_bing_chilling (input: &str) -> IResult<&str, Statement> {
-    let (input, (_, _, _)) = tuple((tag("bing"), space0, tag("\"chilling\"")))(input)?;
-    Ok((input, Statement::Bing))
+fn parse_bing_chilling (i: &str) -> IResult<&str, Statement> {
+    let (i, (_, _, _)) = tuple((tag("bing"), space0, tag("\"chilling\"")))(i)?;
+    Ok((i, Statement::Bing))
 }
 
-fn parse_operation(input: &str) -> IResult<&str, Statement> {
+fn parse_operation(i: &str) -> IResult<&str, Statement> {
     // Opcode | Operand
-
+    let (i, opcode) = parse_opcode(i)?;
+    let (i, operand) = parse_operand(i)?;
     !unimplemented!()
+}
+
+//Immediate     LDA #$44      $A9  2   2
+//Zero Page     LDA $44       $A5  2   3
+//Zero Page,X   LDA $44,X     $B5  2   4
+//Absolute      LDA $4400     $AD  3   4
+//Absolute,X    LDA $4400,X   $BD  3   4+
+//Absolute,Y    LDA $4400,Y   $B9  3   4+
+//Indirect,X    LDA ($44,X)   $A1  2   6
+//Indirect,Y    LDA ($44),Y   $B1  2   5+
+//enum Operand {
+//    // Different adressing modes
+//    Implied,        //
+//    Immediate(u8),  //#$44
+//    ZeroPage(u8),   //$44
+//    ZeroPageX(u8),  //$44,X
+//    Absolute(u16),  //$4400
+//    AbsoluteX(u16), //$4400,X
+//    AbsoluteY(u16), //$4400,Y
+//    IndirectX(u16), //($44,X)
+//    IndirectY(u16), //($44),Y
+//}
+fn parse_operand(i: &str) -> IResult<&str, Operand> {
+    alt((
+        value(Operand::Immediate(8), tag("\n")),
+    ))(i)?;
+    !unimplemented!()
+}
+
+fn parse_immediate(i: &str) -> IResult<&str, Operand> {
+    let (i, _) = tag("#")(i)?;
+    let (i, val) = parse_u8(i)?;
+    Ok((i, Operand::Immediate(val)))
+}
+
+fn parse_opcode(i: &str) -> IResult<&str, Opcode> {
+    alt((
+        value(Opcode::LDA, tag("LDA")),
+        value(Opcode::STA, tag("STA"))
+    ))(i)
 }
 
 const TEST_1: &str = "#2F14DF";
@@ -171,5 +185,86 @@ fn main() {
 
 #[test]
 fn parse_color() {
+}
+
+
+// Utility
+
+// Any number of spaces, newlines and comment
+fn parse_multispace_comment(i: &str) -> IResult<&str, ()> {
+    let (i, _) = many0(alt((value((), multispace1), parse_comment)))(i)?;
+    Ok((i, ()))
+}
+
+// Taken from the nom documentation
+// https://docs.rs/nom/latest/nom/recipes/index.html#comments
+fn parse_comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, (), E> {
+  value(
+    (), // Output is thrown away.
+    pair(char('%'), is_not("\n\r"))
+  )(i)
+}
+
+// Modified from the nom documentation: 
+// https://docs.rs/nom/latest/nom/recipes/index.html#hexadecimal
+fn parse_hex(i: &str) -> IResult<&str, &str> {
+  preceded(
+    tag("$"),
+    recognize(
+      many1(
+        terminated(one_of("0123456789abcdefABCDEF"), many0(char('_')))
+      )
+    )
+  )(i)
+}
+
+fn parse_bin(i: &str) -> IResult<&str, &str> {
+  preceded(
+    tag("%"),
+    recognize(
+      many1(
+        terminated(one_of("01"), many0(char('_')))
+      )
+    )
+  )(i)
+}
+
+fn parse_u8(i: &str) -> IResult<&str, u8> {
+    alt((
+        map_res(  // Base 16
+          parse_hex,
+          |out: &str| u8::from_str_radix(&str::replace(&out, "_", ""), 16)
+        ),
+        map_res(  // Base 2
+          parse_bin,
+          |out: &str| u8::from_str_radix(&str::replace(&out, "_", ""), 2)
+        ),
+        u8,  // Base 10
+    ))(i)
+}
+
+fn parse_u16(i: &str) -> IResult<&str, u16> {
+    alt((
+        map_res(  // Base 16
+          parse_hex,
+          |out: &str| u16::from_str_radix(&str::replace(&out, "_", ""), 16)
+        ),
+        map_res(  // Base 2
+          parse_bin,
+          |out: &str| u16::from_str_radix(&str::replace(&out, "_", ""), 2)
+        ),
+        u16,  // Base 10
+    ))(i)
+}
+
+// Taken from the nom documentation: 
+// https://docs.rs/nom/latest/nom/recipes/index.html#rust-style-identifiers
+pub fn parse_identifier(i: &str) -> IResult<&str, &str> {
+  recognize(
+    pair(
+      alt((alpha1, tag("_"))),
+      many0_count(alt((alphanumeric1, tag("_"))))
+    )
+  )(i)
 }
 
