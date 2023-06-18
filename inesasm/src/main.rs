9c8b3a7f67899b12a73a65cbc8a96c7e3b209456
Rakarake <williambodin2001@gmail.com>
2023-06-18 16:41:05 +0200

    Added at symbol for explicit zero-page

--- inesasm/src/main.rs
+++ inesasm/src/main.rs
@@ -1,3 +1,7 @@
+// Super amazing NES assembler.
+// Zero-Page instructions are prefixed with an '@' before
+// the rest of the operand.
+
 use nom::{
     branch::alt,
     bytes::complete::is_not,
@@ -16,6 +20,7 @@ use nom::{
     combinator::opt,
     combinator::recognize,
     combinator::value,
+    combinator::success,
     error::ParseError,
     multi::many0,
     multi::many0_count,
@@ -28,6 +33,7 @@ use nom::{
 
 use nom_supreme::error::ErrorTree;
 use nom_supreme::tag::complete::tag;
+use nom_supreme::tag::complete::tag_no_case;
 
 use nom_supreme::parser_ext::ParserExt;
 use nom::error::context;
@@ -53,8 +59,8 @@ enum Operand {
     Absolute(u16),  //$4400
     AbsoluteX(u16), //$4400,X
     AbsoluteY(u16), //$4400,Y
-    IndirectX(u16), //($44,X)
-    IndirectY(u16), //($44),Y
+    IndirectX(u8), //($44,X)
+    IndirectY(u8), //($44),Y
 }
 
 #[derive(Debug, Clone)]
@@ -139,23 +145,41 @@ fn parse_operation(i: &str) -> IResult<&str, Statement, ErrorTree<&str>> {
     // Opcode | Operand
     let (i, opcode) = parse_opcode(i)?;
     let (i, operand) = alt((preceded(space1, parse_operand(opcode.clone())), 
-            value(Operand::Implied, parse_multispace_comment)))(i)?;
+            success(Operand::Implied)))(i)?;
+
+    dbg!(operand.clone());
     Ok((i, Statement::Operation(opcode, operand)))
 }
 
+// Operands
 fn parse_operand(opcode: Opcode) -> Box<dyn Fn(&str) -> IResult<&str, Operand, ErrorTree<&str>>> {
     // Match on opcode
     Box::new(move|i: &str| match opcode {
-        Opcode::LDA => alt((parse_immediate, parse_immediate))(i),
-        Opcode::STA => parse_immediate(i),
+        Opcode::LDA => parse_all_addressing_modes(i),
+        Opcode::STA => parse_all_addressing_modes(i),
         Opcode::NOP => parse_implied(i),
     })
 }
 
+// Addressing modes
+//Implied,        //
+//Immediate(u8),  //#$44
+//Absolute(u16),  //$4400
+//AbsoluteX(u16), //$4400,X
+//AbsoluteY(u16), //$4400,Y
+//ZeroPage(u8),   //$44
+//ZeroPageX(u8),  //$44,X
+//IndirectY(u8), //($44),Y
+//IndirectX(u8), //($44,X)
 
-// Operands
+// Parse all addressing modes except for implied 😀
+fn parse_all_addressing_modes(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
+    alt((parse_immediate,
+         parse_absolute_x, parse_absolute_y, parse_absolute,
+         parse_zero_page_x, parse_zero_page,
+         parse_indirect_x, parse_indirect_y))(i)
+}
 
-// Use last in 'alt'
 fn parse_implied(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
     Ok((i, Operand::Implied))
 }
@@ -166,6 +190,68 @@ fn parse_immediate(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
     Ok((i, Operand::Immediate(val)))
 }
 
+fn parse_zero_page(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
+    let (i, _) = tag("@")(i)?;
+    let (i, val) = parse_u8(i)?;
+    Ok((i, Operand::ZeroPage(val)))
+}
+
+fn parse_zero_page_x(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
+    let (i, _) = tag("@")(i)?;
+    let (i, val) = parse_u8(i)?;
+    let (i, _) = parse_trailing_x_y("X")(i)?;
+    Ok((i, Operand::ZeroPageX(val)))
+}
+
+fn parse_absolute(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
+    let (i, val) = parse_u16(i)?;
+    Ok((i, Operand::Absolute(val)))
+}
+
+fn parse_absolute_x(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
+    let (i, val) = parse_u16(i)?;
+    let (i, _) = parse_trailing_x_y("X")(i)?;
+    Ok((i, Operand::AbsoluteX(val)))
+}
+
+fn parse_absolute_y(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
+    let (i, val) = parse_u16(i)?;
+    let (i, _) = parse_trailing_x_y("Y")(i)?;
+    Ok((i, Operand::AbsoluteY(val)))
+}
+
+fn parse_indirect_x(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
+    let (i, _) = tag("(")(i)?;
+    let (i, val) = parse_u8(i)?;
+    let (i, _) = parse_trailing_x_y("X")(i)?;
+    let (i, _) = space0(i)?;
+    let (i, _) = tag(")")(i)?;
+    Ok((i, Operand::IndirectX(val)))
+}
+
+fn parse_indirect_y(i: &str) -> IResult<&str, Operand, ErrorTree<&str>> {
+    let (i, _) = tag("(")(i)?;
+    let (i, val) = parse_u8(i)?;
+    let (i, _) = tag(")")(i)?;
+    let (i, _) = parse_trailing_x_y("Y")(i)?;
+    let (i, _) = space0(i)?;
+    Ok((i, Operand::IndirectY(val)))
+}
+
+//IndirectX(u16), //($44,X)
+//IndirectY(u16), //($44),Y
+
+fn parse_trailing_x_y(x_or_y: &'static str) -> Box<dyn Fn(&str) -> IResult<&str, (), ErrorTree<&str>>> {
+    Box::new(move|i: &str| {
+        let (i, _) = space0(i)?;
+        let (i, _) = tag(",")(i)?;
+        let (i, _) = space0(i)?;
+        let (i, _) = tag_no_case(x_or_y)(i)?;
+        Ok((i, ()))
+    })
+}
+
+// Opcodes
 fn parse_opcode(i: &str) -> IResult<&str, Opcode, ErrorTree<&str>> {
     alt((
         value(Opcode::LDA, tag("LDA")),
@@ -173,22 +259,6 @@ fn parse_opcode(i: &str) -> IResult<&str, Opcode, ErrorTree<&str>> {
         value(Opcode::NOP, tag("NOP")),
     ))(i)
 }
-
-const TEST_1: &str = "#2F14DF";
-
-const TEST_2: &str = "
-wowzers:   NOP
-wowzers2:  NOP
-lad:       LDA #$30
-";
-//TAX             ; Transfer A to X
-//STX $FFFF,X
-fn main() {
-    println!("{:?}", parse_program(TEST_2))
-}
-
-#[test]
-fn parse_color() {}
 
 // Utility
 
@@ -226,6 +296,7 @@ fn parse_bin(i: &str) -> IResult<&str, &str, ErrorTree<&str>> {
     )(i)
 }
 
+//TODO: add digit checking, bases 16=1-2, 10=1-3, 2=1-8
 fn parse_u8(i: &str) -> IResult<&str, u8, ErrorTree<&str>> {
     alt((
         map_res(
@@ -242,6 +313,7 @@ fn parse_u8(i: &str) -> IResult<&str, u8, ErrorTree<&str>> {
     ))(i)
 }
 
+//TODO: add digit checking, bases 16=3-4, 10=4-?, 2=9-16
 fn parse_u16(i: &str) -> IResult<&str, u16, ErrorTree<&str>> {
     alt((
         map_res(
@@ -266,3 +338,34 @@ pub fn parse_identifier(i: &str) -> IResult<&str, &str, ErrorTree<&str>> {
         many0_count(alt((alphanumeric1, tag("_")))),
     ))(i)
 }
+
+
+fn main() {
+    dbg!(parse_program(TEST));
+}
+
+
+
+#[test]
+fn addressing_modes() {
+    let result = parse_program(TEST);
+    match result {
+        Err(error) => panic!("addressing mode parsing error: {:?}", error),
+        Ok((_, Program::Program(statements))) => { 
+            for statement in statements {
+                //TODO: check that every addressing mode is used
+            }
+        },
+    }
+}
+
+const TEST: &str = "
+wowzers:    NOP
+WOWZERS2:   LDA $2000,X
+            LDA ($20, X)
+            LDA ($2F), Y
+ZEROPAGEX:  LDA @$FF,   X
+            LDA $0000,Y
+lad:        LDA #$30
+";
+
