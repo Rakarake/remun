7e6c4e257f445f67deb4ebbabb61d605161f6991
Rakarake <raka@rakarake.xyz>
2025-01-17 21:32:15 +0100

    Addressing modes in table

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,11 @@ impl Opcode {
 // Addressing modes
 enum AddressingMode {
     IMPL,  // nuh uh
+    IMM,   // #FF
     REL,   // $FF
     ABS,   // $FFFF
     ABS_X, // $FFFF,X
     ABS_Y, // $FFFF,Y
-    IMM,   // #FF
     IND,   // ($FFFF)
     X_IND, // ($FF,X)
     IND_Y, // ($FF),Y
@@ -73,22 +73,22 @@ mod instruction_stuff {
     use crate::Instruction;
 
     const INSTRUCTIONS: [Instruction; 256] = [
-    BRK impl ; ORA X,ind ; JAM   ; SLO X,ind ; NOP zpg   ; ORA zpg   ; ASL zpg   ; SLO zpg   ; PHP impl ; ORA #     ;  ASL A    ; ANC #     ; NOP abs   ; ORA abs   ; ASL abs   ; SLO abs   ;
-    BPL rel  ; ORA ind_Y ; JAM   ; SLO ind_Y ; NOP zpg_X ; ORA zpg_X ; ASL zpg_X ; SLO zpg_X ; CLC impl ; ORA abs_Y ;  NOP impl ; SLO abs_Y ; NOP abs_X ; ORA abs_X ; ASL abs_X ; SLO abs_X ;
-    JSR abs  ; AND X,ind ; JAM   ; RLA X,ind ; BIT zpg   ; AND zpg   ; ROL zpg   ; RLA zpg   ; PLP impl ; AND #     ;  ROL A    ; ANC #     ; BIT abs   ; AND abs   ; ROL abs   ; RLA abs   ;
-    BMI rel  ; AND ind_Y ; JAM   ; RLA ind_Y ; NOP zpg_X ; AND zpg_X ; ROL zpg_X ; RLA zpg_X ; SEC impl ; AND abs_Y ;  NOP impl ; RLA abs_Y ; NOP abs_X ; AND abs_X ; ROL abs_X ; RLA abs_X ;
-    RTI impl ; EOR X,ind ; JAM   ; SRE X,ind ; NOP zpg   ; EOR zpg   ; LSR zpg   ; SRE zpg   ; PHA impl ; EOR #     ;  LSR A    ; ALR #     ; JMP abs   ; EOR abs   ; LSR abs   ; SRE abs   ;
-    BVC rel  ; EOR ind_Y ; JAM   ; SRE ind_Y ; NOP zpg_X ; EOR zpg_X ; LSR zpg_X ; SRE zpg_X ; CLI impl ; EOR abs_Y ;  NOP impl ; SRE abs_Y ; NOP abs_X ; EOR abs_X ; LSR abs_X ; SRE abs_X ;
-    RTS impl ; ADC X,ind ; JAM   ; RRA X,ind ; NOP zpg   ; ADC zpg   ; ROR zpg   ; RRA zpg   ; PLA impl ; ADC #     ;  ROR A    ; ARR #     ; JMP ind   ; ADC abs   ; ROR abs   ; RRA abs   ;
-    BVS rel  ; ADC ind_Y ; JAM   ; RRA ind_Y ; NOP zpg_X ; ADC zpg_X ; ROR zpg_X ; RRA zpg_X ; SEI impl ; ADC abs_Y ;  NOP impl ; RRA abs_Y ; NOP abs_X ; ADC abs_X ; ROR abs_X ; RRA abs_X ;
-    NOP #    ; STA X,ind ; NOP # ; SAX X,ind ; STY zpg   ; STA zpg   ; STX zpg   ; SAX zpg   ; DEY impl ; NOP #     ;  TXA impl ; ANE #     ; STY abs   ; STA abs   ; STX abs   ; SAX abs   ;
-    BCC rel  ; STA ind_Y ; JAM   ; SHA ind_Y ; STY zpg_X ; STA zpg_X ; STX zpg_Y ; SAX zpg_Y ; TYA impl ; STA abs_Y ;  TXS impl ; TAS abs_Y ; SHY abs_X ; STA abs_X ; SHX abs_Y ; SHA abs_Y ;
-    LDY #    ; LDA X,ind ; LDX # ; LAX X,ind ; LDY zpg   ; LDA zpg   ; LDX zpg   ; LAX zpg   ; TAY impl ; LDA #     ;  TAX impl ; LXA #     ; LDY abs   ; LDA abs   ; LDX abs   ; LAX abs   ;
-    BCS rel  ; LDA ind_Y ; JAM   ; LAX ind_Y ; LDY zpg_X ; LDA zpg_X ; LDX zpg_Y ; LAX zpg_Y ; CLV impl ; LDA abs_Y ;  TSX impl ; LAS abs_Y ; LDY abs_X ; LDA abs_X ; LDX abs_Y ; LAX abs_Y ;
-    CPY #    ; CMP X,ind ; NOP # ; DCP X,ind ; CPY zpg   ; CMP zpg   ; DEC zpg   ; DCP zpg   ; INY impl ; CMP #     ;  DEX impl ; SBX #     ; CPY abs   ; CMP abs   ; DEC abs   ; DCP abs   ;
-    BNE rel  ; CMP ind_Y ; JAM   ; DCP ind_Y ; NOP zpg_X ; CMP zpg_X ; DEC zpg_X ; DCP zpg_X ; CLD impl ; CMP abs_Y ;  NOP impl ; DCP abs_Y ; NOP abs_X ; CMP abs_X ; DEC abs_X ; DCP abs_X ;
-    CPX #    ; SBC X,ind ; NOP # ; ISC X,ind ; CPX zpg   ; SBC zpg   ; INC zpg   ; ISC zpg   ; INX impl ; SBC #     ;  NOP impl ; USBC #    ; CPX abs   ; SBC abs   ; INC abs   ; ISC abs   ;
-    BEQ rel  ; SBC ind_Y ; JAM   ; ISC ind_Y ; NOP zpg_X ; SBC zpg_X ; INC zpg_X ; ISC zpg_X ; SED impl ; SBC abs_Y ;  NOP impl ; ISC abs_Y ; NOP abs_X ; SBC abs_X ; INC abs_X ; ISC abs_X ;
+    BRK IMPL ; ORA X,IND ; JAM     ; SLO X,IND ; NOP ZPG   ; ORA ZPG   ; ASL ZPG   ; SLO ZPG   ; PHP IMPL ; ORA IMM   ;  ASL A    ; ANC IMM   ; NOP ABS   ; ORA ABS   ; ASL ABS   ; SLO ABS   ;
+    BPL REL  ; ORA IND_Y ; JAM     ; SLO IND_Y ; NOP ZPG_X ; ORA ZPG_X ; ASL ZPG_X ; SLO ZPG_X ; CLC IMPL ; ORA ABS_Y ;  NOP IMPL ; SLO ABS_Y ; NOP ABS_X ; ORA ABS_X ; ASL ABS_X ; SLO ABS_X ;
+    JSR ABS  ; AND X,IND ; JAM     ; RLA X,IND ; BIT ZPG   ; AND ZPG   ; ROL ZPG   ; RLA ZPG   ; PLP IMPL ; AND IMM   ;  ROL A    ; ANC IMM   ; BIT ABS   ; AND ABS   ; ROL ABS   ; RLA ABS   ;
+    BMI REL  ; AND IND_Y ; JAM     ; RLA IND_Y ; NOP ZPG_X ; AND ZPG_X ; ROL ZPG_X ; RLA ZPG_X ; SEC IMPL ; AND ABS_Y ;  NOP IMPL ; RLA ABS_Y ; NOP ABS_X ; AND ABS_X ; ROL ABS_X ; RLA ABS_X ;
+    RTI IMPL ; EOR X,IND ; JAM     ; SRE X,IND ; NOP ZPG   ; EOR ZPG   ; LSR ZPG   ; SRE ZPG   ; PHA IMPL ; EOR IMM   ;  LSR A    ; ALR IMM   ; JMP ABS   ; EOR ABS   ; LSR ABS   ; SRE ABS   ;
+    BVC REL  ; EOR IND_Y ; JAM     ; SRE IND_Y ; NOP ZPG_X ; EOR ZPG_X ; LSR ZPG_X ; SRE ZPG_X ; CLI IMPL ; EOR ABS_Y ;  NOP IMPL ; SRE ABS_Y ; NOP ABS_X ; EOR ABS_X ; LSR ABS_X ; SRE ABS_X ;
+    RTS IMPL ; ADC X,IND ; JAM     ; RRA X,IND ; NOP ZPG   ; ADC ZPG   ; ROR ZPG   ; RRA ZPG   ; PLA IMPL ; ADC IMM   ;  ROR A    ; ARR IMM   ; JMP IND   ; ADC ABS   ; ROR ABS   ; RRA ABS   ;
+    BVS REL  ; ADC IND_Y ; JAM     ; RRA IND_Y ; NOP ZPG_X ; ADC ZPG_X ; ROR ZPG_X ; RRA ZPG_X ; SEI IMPL ; ADC ABS_Y ;  NOP IMPL ; RRA ABS_Y ; NOP ABS_X ; ADC ABS_X ; ROR ABS_X ; RRA ABS_X ;
+    NOP IMM  ; STA X,IND ; NOP IMM ; SAX X,IND ; STY ZPG   ; STA ZPG   ; STX ZPG   ; SAX ZPG   ; DEY IMPL ; NOP IMM   ;  TXA IMPL ; ANE IMM   ; STY ABS   ; STA ABS   ; STX ABS   ; SAX ABS   ;
+    BCC REL  ; STA IND_Y ; JAM     ; SHA IND_Y ; STY ZPG_X ; STA ZPG_X ; STX ZPG_Y ; SAX ZPG_Y ; TYA IMPL ; STA ABS_Y ;  TXS IMPL ; TAS ABS_Y ; SHY ABS_X ; STA ABS_X ; SHX ABS_Y ; SHA ABS_Y ;
+    LDY IMM  ; LDA X,IND ; LDX IMM ; LAX X,IND ; LDY ZPG   ; LDA ZPG   ; LDX ZPG   ; LAX ZPG   ; TAY IMPL ; LDA IMM   ;  TAX IMPL ; LXA IMM   ; LDY ABS   ; LDA ABS   ; LDX ABS   ; LAX ABS   ;
+    BCS REL  ; LDA IND_Y ; JAM     ; LAX IND_Y ; LDY ZPG_X ; LDA ZPG_X ; LDX ZPG_Y ; LAX ZPG_Y ; CLV IMPL ; LDA ABS_Y ;  TSX IMPL ; LAS ABS_Y ; LDY ABS_X ; LDA ABS_X ; LDX ABS_Y ; LAX ABS_Y ;
+    CPY IMM  ; CMP X,IND ; NOP IMM ; DCP X,IND ; CPY ZPG   ; CMP ZPG   ; DEC ZPG   ; DCP ZPG   ; INY IMPL ; CMP IMM   ;  DEX IMPL ; SBX IMM   ; CPY ABS   ; CMP ABS   ; DEC ABS   ; DCP ABS   ;
+    BNE REL  ; CMP IND_Y ; JAM     ; DCP IND_Y ; NOP ZPG_X ; CMP ZPG_X ; DEC ZPG_X ; DCP ZPG_X ; CLD IMPL ; CMP ABS_Y ;  NOP IMPL ; DCP ABS_Y ; NOP ABS_X ; CMP ABS_X ; DEC ABS_X ; DCP ABS_X ;
+    CPX IMM  ; SBC X,IND ; NOP IMM ; ISC X,IND ; CPX ZPG   ; SBC ZPG   ; INC ZPG   ; ISC ZPG   ; INX IMPL ; SBC IMM   ;  NOP IMPL ; USBC IMM  ; CPX ABS   ; SBC ABS   ; INC ABS   ; ISC ABS   ;
+    BEQ REL  ; SBC IND_Y ; JAM     ; ISC IND_Y ; NOP ZPG_X ; SBC ZPG_X ; INC ZPG_X ; ISC ZPG_X ; SED IMPL ; SBC ABS_Y ;  NOP IMPL ; ISC ABS_Y ; NOP ABS_X ; SBC ABS_X ; INC ABS_X ; ISC ABS_X ;
     ];
 }
 
