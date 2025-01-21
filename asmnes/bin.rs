47135fdacdba2f14e8784511975073317c1a52f1
Rakarake <raka@rakarake.xyz>
2025-01-21 19:32:38 +0100

    gah

--- asmnes/bin.rs
+++ asmnes/bin.rs
@@ -1,5 +1,7 @@
 use shared::INSTRUCTIONS;
+use asmnes::MORG;
 
 fn main() {
     println!("hello!");
 }
+
---
+++ asmnes/lib.rs
@@ -0,0 +1,9 @@
+use shared::Opcode;
+use shared::AddressingMode;
+
+pub const MORG: u32 = 3;
+
+pub fn asseble(input: String) -> Vec<u8> {
+    return vec![]
+}
+
