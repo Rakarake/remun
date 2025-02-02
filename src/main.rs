fcef6c634de0ee390453a6f61664d286c634c582
Rakarake <raka@rakarake.xyz>
2025-02-02 15:29:08 +0100

    uhm, seems to work

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use shared::AddressingMode::*;
 use asmnes::Operand::*;
 use asmnes::INSTR;
 use asmnes::INSTRL;
+use asmnes::Directive;
 use remun::State;
-use remun::Range;
 
 // TODO make asmnes program struct, takes in ines or (prg, debug, char)? (no, depends on mappers)
 // new_form_regions(regions, debug)
@@ -14,21 +14,19 @@ use remun::Range;
 fn main() -> Result<(), AsmnesError> {
     let AsmnesOutput { program, labels } = asmnes::logical_assemble_plus(&[
         INSTRL::INSTR(INSTR(LDA, IMM, U8(0x02))),
-        INSTRL::LABEL("HELLO_WORLD".to_string()),
-        INSTRL::INSTR(INSTR(LDA, IMM, U8(0x02))),
-        INSTRL::INSTR(INSTR(STA, ABS, Label("HELLO_WORLD".to_string()))),
-        INSTRL::INSTR(INSTR(LDX, ABS, Label("HELLO_WORLD".to_string()))),
+        INSTRL::INSTR(INSTR(STA, ABS, Label("VAR_A".to_string()))),
+        INSTRL::INSTR(INSTR(LDX, ABS, Label("VAR_A".to_string()))),
+        INSTRL::DIR(Directive::RS(2)),
+        INSTRL::LABEL("VAR_A".to_string()),
+        INSTRL::DIR(Directive::RS(4)),
+        INSTRL::LABEL("VAR_B".to_string()),
     ])?;
     let mut state = State::new_nrom128(program.clone(), program);
     println!("labels: {:?}", labels);
     state.run_one_instruction();
-    println!("a: {}", state.a);
-    state.run_one_instruction();
     state.run_one_instruction();
     state.run_one_instruction();
     state.print_state();
-    println!("addr 2!? {}", state.read(2)); // should be HELLO_WORLD label
-    println!("{:?}", state.x);
     Ok(())
 }
 
