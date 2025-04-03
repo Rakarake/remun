5f96a4c8d2ba2c548394ea1d3b909e3e954f04c2
Rakarake <raka@rakarake.xyz>
2025-04-03 17:39:12 +0200

    big crate restructure, made a separate crate to separate gui

---
+++ remunapp/app.rs
@@ -0,0 +1,29 @@
+use remun::State;
+
+//mod visualizer;
+
+pub fn run(mut state: State) {
+    //let options = eframe::NativeOptions {
+    //    viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+    //    ..Default::default()
+    //};
+    //eframe::run_native(
+    //    "Remnun debugger",
+    //    options,
+    //    Box::new(|cc| {
+    //        // This gives us image support:
+    //        egui_extras::install_image_loaders(&cc.egui_ctx);
+    //        let disassembly = asmnes::disassemble(&(0..=u16::MAX).map(|a| state.read(a, true)).collect::<Vec<u8>>()).0
+    //            .iter().map(|(a, i)| (*a as u16, i.clone())).collect();
+    //        Ok(Box::new(MyApp {
+    //            state,
+    //            running: false,
+    //            speed: 1,
+    //            scroll: 0xC000,
+    //            following_pc: true,
+    //            disassembly,
+    //        }))
+    //    }),
+    //)
+}
+
--- remunapp/main.rs
+++ remunapp/main.rs
@@ -1,12 +1,14 @@
 #![feature(let_chains)]
 use std::{env, error::Error, path::Path};
 
+use remun;
+
 mod app;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let ines = remun::load_from_file(env::args().nth(1).ok_or("please give file as argument")?)?;
     let state = remun::State::new(ines);
-    app::run(state)?;
+    app::run(state);
     Ok(())
 }
