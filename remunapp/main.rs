dd0f5fc79708e0465ab9f821c257a389e5e7e92a
Rakarake <raka@rakarake.xyz>
2025-04-03 19:09:52 +0200

    progress

--- remunapp/main.rs
+++ remunapp/main.rs
@@ -1,13 +1,29 @@
 #![feature(let_chains)]
+use remun::State;
 use std::{env, error::Error, path::Path};
+use winit::{
+    dpi::PhysicalSize,
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
 
-mod app;
 mod visualizer;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let ines = remun::load_from_file(env::args().nth(1).ok_or("please give file as argument")?)?;
     let state = remun::State::new(ines);
-    app::run(state);
     Ok(())
+}
+
+fn create_window(event_loop: &EventLoop<()>) -> (Window, PhysicalSize<u32>) {
+    let size = PhysicalSize::new(400, 400);
+    (
+        WindowBuilder::new()
+            .with_inner_size(size)
+            .build(&event_loop)
+            .unwrap(),
+        size,
+    )
 }
