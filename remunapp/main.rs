3d77e8314bb3d0183e6cf41dd947a87abd0a26a5
Rakarake <raka@rakarake.xyz>
2025-04-11 20:15:30 +0200

    perfect square

--- remunapp/main.rs
+++ remunapp/main.rs
@@ -67,19 +67,31 @@ impl Vertex {
     }
 }
 
+//const VERTICES: &[Vertex] = &[
+//    // Changed
+//    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
+//    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], }, // B
+//    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397], }, // C
+//    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914], }, // D
+//    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], }, // E
+//];
+//
+//const INDICES: &[u16] = &[
+//    0, 1, 4,
+//    1, 2, 4,
+//    2, 3, 4,
+//];
 const VERTICES: &[Vertex] = &[
-    // Changed
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], }, // B
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397], }, // C
-    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914], }, // D
-    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], }, // E
+    // top left
+    Vertex { position: [-1., 1., 0.0], tex_coords: [0.0, 0.0], },
+    Vertex { position: [-1., -1., 0.0], tex_coords: [0.0, 1.0], },
+    Vertex { position: [1., -1., 0.0], tex_coords: [1.0, 1.0], },
+    Vertex { position: [1., 1., 0.0], tex_coords: [1.0, 0.0], },
 ];
 
 const INDICES: &[u16] = &[
-    0, 1, 4,
-    1, 2, 4,
-    2, 3, 4,
+    0, 1, 2,
+    2, 3, 0,
 ];
 
 impl App<'_> {
@@ -400,13 +412,13 @@ impl RenderState<'_> {
                 let mut b0 = *tile[i];
                 let mut b1 = *tile[i + 8];
                 // generate 8 colors
-                for j in 0..8 {
+                for _ in 0..8 {
                     let rgba = color_lookup[((b0 & 0b00000001) | ((b1 & 0b00000001) << 1)) as usize].to_be_bytes();
-                    b0 >>= 1;
-                    b1 >>= 1;
                     for c in rgba {
                         acc.push(c);
                     }
+                    b0 >>= 1;
+                    b1 >>= 1;
                 }
             }
             acc
